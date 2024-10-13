2abbc8914dcbe3ceae2d5e2d94e9436a9ae9e2c6
Sanchit Mishra <sanchit1417@gmail.com>
2024-10-13 14:59:35 +0530

    added error decoder

--- OrderService/src/main/java/com/example/OrderService/Exception/RestResponseEntityException.java
+++ OrderService/src/main/java/com/example/OrderService/Exception/RestResponseEntityException.java
@@ -1,6 +1,7 @@
 package com.example.OrderService.Exception;
 
 import com.example.OrderService.External.Response.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,5 +11,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class RestResponseEntityException extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponse>
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException exception){
+        return new ResponseEntity<>(new ErrorResponse().builder()
+                .errorMessage(exception.getMessage())
+                .errorCode(exception.getErrorCode()).build(), HttpStatus.valueOf(exception.getStatus()));
+    }
 }
